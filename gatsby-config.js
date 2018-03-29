6f62106d121d052551ce8b5cbb07112cbabf04f6
Joe Dahle <joe.dahle@gmail.com>
2018-03-28 18:14:55 -0700

    rm: Removed blog elements

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,13 +25,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/posts`,
-        name: 'posts',
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         name: `images`,
         path: `${__dirname}/src/assets/images`,
       },
