6b79eaea904c2f6c2a505c3b56f43991ce76b209
Joe Dahle <joe.dahle@gmail.com>
2018-06-12 16:36:08 -0700

    refactor: Rewrite of navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,53 +1,15 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import Navigator from './Navigator'
+
 import logo from '../assets/images/27oz_logo_text_black_xl.png'
 import subLogo from '../assets/images/27oz_logo_pink_transparent.png'
 
 const Header = props => (
   <header id="header" className="alt">
-    <div className="navigation">
-      <input
-        type="checkbox"
-        className="navigation__checkbox"
-        id="navi-toggle"
-      />
-      <label htmlFor="navi-toggle" className="navigation__button">
-        <span className="navigation__icon">&nbsp;</span>
-      </label>
+    <Navigator />
 
-      <div className="navigation__background">&nbsp;</div>
-
-      <nav className="navigation__nav">
-        <ul className="navigation__list">
-          <li className="navigation__item">
-            <Link to="/" className="navigation__link">
-              Home
-            </Link>
-          </li>
-          <li className="navigation__item">
-            <Link to="/cloudBackups" className="navigation__link">
-              Cloud Backups
-            </Link>
-          </li>
-          <li className="navigation__item">
-            <Link to="itSupport" className="navigation__link">
-              IT Support
-            </Link>
-          </li>
-          <li className="navigation__item">
-            <Link to="crypto" className="navigation__link">
-              Cryptomining
-            </Link>
-          </li>
-          <li className="navigation__item">
-            <a href="https://blog.27oz.io" className="navigation__link">
-              Blog
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </div>
     <span className="logo">
       <img style={{ maxWidth: '6em' }} src={subLogo} alt="" />
     </span>
---
+++ src/components/Navigator.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react'
+import Link from 'gatsby-link'
+
+class Navigator extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      navIsOpen: false,
+    }
+
+    this.handleOnClick = this.handleOnClick.bind(this)
+  }
+
+  handleOnClick(e) {
+    this.setState({
+      navIsOpen: !this.state.navIsOpen,
+    })
+  }
+
+  render() {
+    return (
+      <div className="navigation" onClick={this.handleOnClick}>
+        <div
+          className={
+            this.state.navIsOpen
+              ? 'navigation__button navigation__button--open'
+              : 'navigation__button'
+          }
+        >
+          <span className="navigation__icon" />
+        </div>
+        <div className="navigation__background">&nbsp;</div>
+        <nav
+          className={
+            this.state.navIsOpen
+              ? 'navigation__nav'
+              : 'navigation__nav navigation__nav--closed'
+          }
+        >
+          <ul className="navigation__list">
+            <li className="navigation__item">
+              <Link to="/" className="navigation__link">
+                Home
+              </Link>
+            </li>
+            <li className="navigation__item">
+              <Link to="/cloudBackups" className="navigation__link">
+                Cloud Backups
+              </Link>
+            </li>
+            <li className="navigation__item">
+              <Link to="itSupport" className="navigation__link">
+                IT Support
+              </Link>
+            </li>
+            <li className="navigation__item">
+              <Link to="crypto" className="navigation__link">
+                Cryptomining
+              </Link>
+            </li>
+            <li className="navigation__item">
+              <a href="https://blog.27oz.io" className="navigation__link">
+                Blog
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    )
+  }
+}
+
+export default Navigator
